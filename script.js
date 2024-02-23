d3c893594f21c62c29d94948c47ff19760321989
Andrew Luca <andrew.d.luca@gmail.com>
2024-02-23 10:39:37 -0800

    Updated javascript to fetch into REST API for user credentials

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-function login() {
-    // Hardcoded credentials for validation
-    var username = "user";
-    var password = "pass";
-
-    // User input from form
+async function login() {
     var inputUsername = document.querySelector('input[name="username"]').value;
     var inputPassword = document.querySelector('input[name="password"]').value;
 
-    // Check if the user input matches the hardcoded credentials
-    if (inputUsername === username && inputPassword === password) {
-        window.location.replace('dashboard.html');
-    } else {
-        alert("Login failed: Incorrect username or password.");
+    try {
+        const response = await fetch('https://xfae0aaxng.execute-api.us-west-2.amazonaws.com/prod/dbapi', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username: inputUsername, password: inputPassword })
+        });
+        
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        
+        const data = await response.json();
+        
+        if (data.success) {
+            window.location.replace('dashboard.html');
+        } else {
+            alert("Login failed: Incorrect username or password.");
+        }
+    } catch (error) {
+        console.error('Login failed:', error);
+        alert("Login failed: An error occurred. Please try again.");
     }
 }
+
 function logout() {
     window.location.replace('loginpage.html');
 }
