1cfc0cdbec35822d8ae1a574d0839406008f59dd
danitzafuenzalidabarriga@gmail.com <danitzafuenzalidabarriga@gmail.com>
2024-02-27 14:13:09 -0800

    have created all necessary files for sign up page creation. further revisions will be done on AWS.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// This function should be called when using loginPage
 async function login() {
     var inputUsername = document.querySelector('input[name="username"]').value;
     var inputPassword = document.querySelector('input[name="password"]').value;
@@ -12,7 +14,7 @@ async function login() {
         });
         
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error('Network response error');
         }
         
         const data = await response.json();
@@ -30,4 +32,38 @@ async function login() {
 
 function logout() {
     window.location.replace('loginpage.html');
+}
+
+
+// This function should be called when using SignIn
+
+async function signUp() {
+
+    var inputUsername = document.querySelector('input[name="username"]').value;
+    var inputPassword = document.querySelector('input[name="password"]').value;
+
+    try {
+        const response = await fetch('https://xfae0aaxng.execute-api.us-west-2.amazonaws.com/prod/createNewUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username: inputUsername, password: inputPassword })
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response error');
+        }
+
+        const data = await response.json();
+
+        if (data.success) {
+            window.location.replace('emailConfirm.html');
+        } else {
+            alert("Sign In Failed: Error on username or password.");
+        }
+    } catch (error) {
+        console.error('Sign In Failed:', error);
+        alert("Sign In Failed: An error occurred. Please try again.");
+    }
 }
